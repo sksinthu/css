bec9983eb1ee53c627a9726086e819e3a18d07f4
sksinthu <sksinthu15@gmail.com>
2022-07-01 22:46:14 +0530

    calculator

---
+++ calculator/myex.js
@@ -0,0 +1,19 @@
+let output=document.getElementById("outputScre");
+function display(num){
+outputScreen.value +=num
+};
+function calculate(){
+try{
+    outputScreen.value=eval(outputScreen.value)
+}
+catch(err)
+{
+    alert("invalid")
+}
+}
+function Clear(){
+    outputScreen.value="";
+}
+function del(){
+    outputScreen.value=outputScreen.value.slice(0,-1);
+}
